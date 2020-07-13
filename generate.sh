5d655f4dc0c679fc169d87363bfc673d1150cb90
Lothar Wieske <lothar.wieske@gmail.com>
2020-07-13 20:50:00 +0200

    Modernization

--- generate.sh
+++ generate.sh
@@ -6,11 +6,11 @@ PREFIX="./logo"
 rm logo*.png
 for i in 48 96 128 144 256 512 1024
 do
-  svg2png lwieske.svg --output=logo${i}px.png --width=${i} --height=${i}
+  svg2png lwieske.svg --width=${i} --height=${i} >>logo${i}px.png
 done
 
 rm design/design*.png
-for i in 1 2 3 4 5 6
+for i in 1 2 3 4 5
 do
-  svg2png design/design${i}.svg --output=design/design${i}.png --width=512 --height=512
+  svg2png design/design${i}.svg --width=512 --height=512 >>design/design${i}.png
 done
