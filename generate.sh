ac3dccceb299d941783bd8f4d1427f715a9658f7
Lothar Wieske <lothar.wieske@gmail.com>
2020-07-26 21:42:30 +0200

    Updating PNGs and generate script

--- generate.sh
+++ generate.sh
@@ -3,14 +3,14 @@
 INPUT="./lwieske.svg"
 PREFIX="./logo"
 
-rm logo*.png
+#rm logo*.png
 for i in 48 96 128 144 256 512 1024
 do
-  svg2png lwieske.svg --width=${i} --height=${i} >>logo${i}px.png
+  inkscape .lwieske.svg -w ${i} -o logo${i}px.png
 done
 
 rm design/design*.png
 for i in 1 2 3 4 5
 do
-  svg2png design/design${i}.svg --width=512 --height=512 >>design/design${i}.png
+  inkscape design/design${i}.svg -w 512 -o design/design${i}.png
 done
