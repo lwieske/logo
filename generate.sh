44f082fe361573a5e73987e471e9caa659e60a0a
Lothar Wieske <lothar.wieske@gmail.com>
2023-11-08 00:56:19 +0100

    rsvg-convert + cropped pngs

--- generate.sh
+++ generate.sh
@@ -3,14 +3,19 @@
 INPUT="./lwieske.svg"
 PREFIX="./logo"
 
-#rm logo*.png
+rm logo*px.png cropped-*.png design/design*.png
+
 for i in 48 96 128 144 256 512 1024
 do
-  inkscape .lwieske.svg -w ${i} -o logo${i}px.png
+  rsvg-convert lwieske.svg --width ${i} --height ${i} --output logo${i}px.png
 done
 
-rm design/design*.png
+for i in 48 96 128 144 256 512 1024
+do
+  convert logo${i}px.png -trim cropped-logo${i}px.png
+done
+
 for i in 1 2 3 4 5
 do
-  inkscape design/design${i}.svg -w 512 -o design/design${i}.png
+  rsvg-convert design/design${i}.svg --width 512 --output design/design${i}.png
 done
